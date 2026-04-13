0eddf396ef38dfb3ad14e0b6cd22ab639713fd35
unknown <tejaskhilare35@gmail.com>
2026-04-13 22:16:04 +0530

    Bucket name changed

--- backend/app/services/s3_service.py
+++ backend/app/services/s3_service.py
@@ -1,23 +1,21 @@
 import boto3
 import uuid
+import os
 
-BUCKET_NAME = "tejas-todo-profile-images"
+BUCKET_NAME = os.getenv("AWS_BUCKET_NAME")
+AWS_REGION = os.getenv("AWS_REGION", "ap-south-1")
 
 s3 = boto3.client("s3")
+
 def upload_file_to_s3(file):
-    try:
-        file_extension = file.filename.split(".")[-1]
-        unique_filename = f"{uuid.uuid4()}.{file_extension}"
+    file_extension = file.filename.split(".")[-1]
+    unique_filename = f"{uuid.uuid4()}.{file_extension}"
 
-        s3.upload_fileobj(
-            file.file,
-            BUCKET_NAME,
-            unique_filename,
-            ExtraArgs={"ContentType": file.content_type}
-        )
+    s3.upload_fileobj(
+        file.file,
+        BUCKET_NAME,
+        unique_filename,
+        ExtraArgs={"ContentType": file.content_type}
+    )
 
-        file_url = f"https://{BUCKET_NAME}.s3.amazonaws.com/{unique_filename}"
-        return file_url
-
-    except Exception as e:
-        raise Exception(f"S3 Upload Failed: {str(e)}")
+    return f"https://{BUCKET_NAME}.s3.{AWS_REGION}.amazonaws.com/{unique_filename}"
