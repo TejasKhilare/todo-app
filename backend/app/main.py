3a8f5a2955596a563eb691645254fdd3405f897c
TejasKhilare <tejaskhilare35@gmail.com>
2026-04-14 06:37:07 +0000

    Allow s3 endpoint preview origin in CORS

--- backend/app/main.py
+++ backend/app/main.py
@@ -49,7 +49,8 @@ app.add_middleware(
         "http://localhost:5173",
         "http://127.0.0.1:5173",
         "http://localhost:4173",
-        "http://127.0.0.1:4173"
+        "http://127.0.0.1:4173",
+        "http://tejas-todo-frontend.s3-website.ap-south-1.amazonaws.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
